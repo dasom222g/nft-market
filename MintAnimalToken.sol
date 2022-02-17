ad3ec2884dc28f490cf232b157b14c2bc5ca48ad
dasom <dasom228@gmail.com>
2022-02-17 19:57:14 +0900

    Complete minting

--- MintAnimalToken.sol
+++ MintAnimalToken.sol
@@ -7,9 +7,18 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract MintAnimalToken is ERC721Enumerable {
     constructor() ERC721("somiAnimals", "SMA") {}
 
+    mapping(uint256 => uint256) public animalTypeMap;
+
     function mintAnimalToken() public {
-        // 유일한 값
-        uint256 animalTokenId = totalSupply() + 1;
+        uint256 animalTokenId = totalSupply() + 1; // 유일한 값
+        // %5 : 0 ~ 4
+        // +1 : 1 ~ 5
+        uint256 animalType = (uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, animalTokenId, msg.sender)
+            )
+        ) % 5) + 1;
+        animalTypeMap[animalTokenId] = animalType;
 
         _mint(msg.sender, animalTokenId); // minting하기
     }
