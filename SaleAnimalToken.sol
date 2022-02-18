aae5b8fc8aed76edb0c5dc67357553ac8cec5607
dasom <dasom228@gmail.com>
2022-02-18 12:34:03 +0900

    Add purchaseToken

--- SaleAnimalToken.sol
+++ SaleAnimalToken.sol
@@ -35,4 +35,39 @@ contract SaleAnimalToken {
         priceMap[_animalTokenId] = _price;
         onSaleAnimalTokenIds.push(_animalTokenId);
     }
+
+    // 구매
+    function purchaseAnimalToken(uint256 _animalTokenId) public payable {
+        uint256 price = priceMap[_animalTokenId];
+        address owner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+        // 1. 판매중인 토큰인지 확인
+        // 2. 거래금액(value)은 책정된 가격보다 작을 수 없음
+        // 3. 소유권자는 구매 불가
+        require(price > 0, "This animal token is not for sale.");
+        require(msg.value >= price, "Caller sent lower than price.");
+        require(owner != msg.sender, "Caller is animal token owner.");
+
+        // 실제 코인거래
+        payable(owner).transfer(msg.value); // 판매자에서 코인 전달
+        mintAnimalTokenAddress.safeTransferFrom(owner, msg.sender, _animalTokenId);
+
+        priceMap[_animalTokenId] = 0;
+        // 판매중 토큰 id 제거
+        for (uint256 i = 0; i < onSaleAnimalTokenIds.length; i++) {
+            if (priceMap[onSaleAnimalTokenIds[i]] == 0) {
+                // 해당 원소를 배열의 마지막원소로 대체후 마지막 원소 제거
+                onSaleAnimalTokenIds[i] = onSaleAnimalTokenIds[
+                    onSaleAnimalTokenIds.length - 1
+                ];
+                onSaleAnimalTokenIds.pop();
+            }
+        }
+    }
+
+    function getOnSaleAnimalTokenIds() public view returns (uint256) {
+        return onSaleAnimalTokenIds.length;
+    }
+    function getOnSaleAnimalTokenPrice(uint256 _animalTokenId) public view returns(uint256) {
+        return priceMap[_animalTokenId];
+    }
 }
